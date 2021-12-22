ad300d4ef412839477328e1a26dd55eea7832b2b
fewpwsp <90150454+fewpwsp@users.noreply.github.com>
2021-12-22 08:40:47 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,40 @@
 
+window.onload = function() {
+    document.getElementById("hide-button-first").onclick = hideFunсFirst;
+    document.getElementById("hide-button-second").onclick = hideFunсSecond;
+    document.getElementById("hide-button-third").onclick = hideFunсThird;
+    document.getElementById("hide-button-fourth").onclick = hideFunсFourth;
+    document.getElementById("drawDiagramm").onclick = DrawDiagramm;
+    
+};
+
+function hideFunсFirst() {
+    if(document.getElementById("hidden-block-first").style.display == "none") {
+        document.getElementById("hidden-block-first").style.display = "block";
+    } else {
+        document.getElementById("hidden-block-first").style.display = "none";
+    }
+}
+function hideFunсSecond() {
+    if(document.getElementById("hidden-block-second").style.display == "none") {
+        document.getElementById("hidden-block-second").style.display = "block";
+    } else {
+        document.getElementById("hidden-block-second").style.display = "none";
+    }
+}
+function hideFunсThird() {
+    if(document.getElementById("hidden-block-third").style.display == "none") {
+        document.getElementById("hidden-block-third").style.display = "block";
+    } else {
+        document.getElementById("hidden-block-third").style.display = "none";
+    }
+}
+function hideFunсFourth() {
+    if(document.getElementById("hidden-block-fourth").style.display == "none") {
+        document.getElementById("hidden-block-fourth").style.display = "block";
+    } else {
+        document.getElementById("hidden-block-fourth").style.display = "none";
+    }
+}
+
+
